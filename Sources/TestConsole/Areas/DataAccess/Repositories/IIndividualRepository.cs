63d72ac8396e02429fbf0df887e35029dde6cc2c
Matthias Mueller <matthiasm@live.de>
2018-12-03 20:41:05 +0100

    WIP

--- Sources/TestConsole/Areas/DataAccess/Repositories/IIndividualRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mmu.Mlh.DataAccess.Rest.TestConsole.Areas.Domain.Models;
-using Mmu.Mlh.DomainExtensions.Areas.Repositories;
-
-namespace Mmu.Mlh.DataAccess.Rest.TestConsole.Areas.DataAccess.Repositories
-{
-    public interface IIndividualRepository : IRepository<Individual, string>
-    {
-    }
-}
